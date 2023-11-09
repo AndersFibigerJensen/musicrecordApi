5bbe1f4ab0539e61951dfc048ebeef00797d2507
Mammon22 <106878588+Mammon22@users.noreply.github.com>
2023-11-09 12:55:36 +0100

    Add project files.

---
+++ musicrecordApi/Models/Record.cs
@@ -0,0 +1,15 @@
+﻿namespace musicrecordApi.Models
+{
+    public class Record
+    {
+        public string Title { get; set; }
+
+        public string Artist { get; set; }
+
+        public int Duration { get; set; }
+
+        public int PublicationYear { get; set; }
+
+        public int Downloads { get; set; }
+    }
+}
---
+++ musicrecordApi/Repo/RecordRepository.cs
@@ -0,0 +1,15 @@
+﻿using musicrecordApi.Models;
+
+namespace musicrecordApi.Repo
+{
+    public class RecordRepository
+    {
+        private List<Record> records = new();
+
+        public RecordRepository()
+        {
+            records.Add(new Record { Artist = "Luthor", Title = "For evigt", Duration = 180, Downloads = 100, PublicationYear = 2004 });
+            
+        }
+    }
+}
