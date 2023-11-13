231134d744c27dc6be2ecfcd65b4f54636ffb87a
thom803m <115149062+thom803m@users.noreply.github.com>
2023-11-13 11:06:10 +0100

    allow all

--- musicrecordApi/Program.cs
+++ musicrecordApi/Program.cs
@@ -7,6 +7,15 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options => {
+    options.AddPolicy(name: "AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
